503c6c94b8e475f3c28822df494c62daf63a01ad
AntonioK180 <antonio.kasabov@gmail.com>
2020-12-13 11:48:15 +0200

    Minor changes from what we talked (10/12/2020)

--- database.py
+++ database.py
@@ -6,11 +6,11 @@ conn = sqlite3.connect(DB_NAME)
 
 
 conn.cursor().execute('''
-    CREATE TABLE IF NOT EXISTS posts 
+    CREATE TABLE IF NOT EXISTS posts
     (
-        id INTEGER PRIMARY KEY AUTOINCREMENT, 
-        name TEXT, 
-        author TEXT, 
+        post_id INTEGER PRIMARY KEY AUTOINCREMENT,
+        name TEXT,
+        author TEXT,
         content TEXT
     )
 ''')
@@ -18,7 +18,7 @@ conn.cursor().execute('''
 conn.cursor().execute('''
 CREATE TABLE IF NOT EXISTS comments
     (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
         post_id INTEGER,
         message TEXT,
         FOREIGN KEY(post_id) REFERENCES posts(id)
